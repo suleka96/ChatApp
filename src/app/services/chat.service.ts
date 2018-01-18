e60776513ffd834ac228b24a6482ba9d855d9952
suleka96 <sulekahelmini96comp@gmail.com>
2018-01-18 13:39:25 +0530

    chat services eddited

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {Observable} from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
+import {AuthService} from '../services/auth.service';
+
+
+import {ChatMessage} from '../models/chat-message.model';
+
 @Injectable()
 export class ChatService {
 
